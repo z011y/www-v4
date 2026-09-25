00d006b310c431612d9377f9c905427b2c235231
Cameron Zollinger <cameron@z011y.dev>
2026-09-24 21:03:50 -0600

    update env var

--- db/db.ts
+++ db/db.ts
@@ -1,8 +1,11 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-const sql =
-  process.env.ENV === "prod"
-    ? neon(process.env.PROD_DATABASE_URL!)
-    : neon(process.env.DEV_DATABASE_URL!);
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  (process.env.ENV === "prod"
+    ? process.env.PROD_DATABASE_URL
+    : process.env.DEV_DATABASE_URL);
+
+const sql = neon(databaseUrl!);
 export const db = drizzle({ client: sql });
